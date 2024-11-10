0e65767646d4c8ae29d222412e217fbaa60de34c
C1ey <105993415+C1ey@users.noreply.github.com>
2024-11-10 15:56:49 -0800

    Update RSA_Decoder.py

--- RSA_Decoder.py
+++ RSA_Decoder.py
@@ -1,6 +1,45 @@
+#Created by: Cleon Simpson
+#Date: 10/11/2024
 import math
 
+#This follows the Euclidean algorithm
 def gcd(a,b):
     while b:
-        a,b=b,a%b
+        a,b=b,a%b #switches the bigger number with the smaller one continuously until one becomes 0
     return a
+
+#The below finds the modular inverse of e%phi
+def mod_inverse(e,phi):
+# 1<e<(a-1)*(b_1)
+    for d in range (1,phi):
+        if (d*e)%phi ==1:
+            return d
+        else:
+            return None
+
+#The below finds the two prime factors a and b       
+def factorize(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if n%i==0:
+            return i,n//i
+    return None,None
+
+
+#The below finds 
+def rsa_decrypt(c,e,n):
+    p,q=factorize(n)
+    if not p or not q:
+        return "Unable to factorize n"
+    
+    #We will need a function to calculate the phi of n
+    phi=(p-1)*(q-1)
+    #We will need a function to calculate the mod inverse
+    d=mod_inverse(e,phi)
+    if not d:
+        return "Failed to find modular Inverse"
+    
+    message=pow(c,d,n)
+    return message
+
+decrypt_message=rsa_decrypt(ciphertext,e,n)
+print("Decrypted Message: ", decrypt_message)
